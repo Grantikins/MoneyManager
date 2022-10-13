0ea0eda6733a555401e54850d6cb561a3ba24ead
Grantikins <grantikinsr@gmail.com>
2022-10-12 22:31:31 -0500

    cleaned up stuff

--- app.js
+++ app.js
@@ -8,11 +8,6 @@ const session = require("express-session")
 const passport = require("./middlewares/passport-config")
 const flash = require("express-flash")
 
-// TEST Stuff
-const User = require("./models/user")
-
-// END TEST
-
 const {database} = require("./dbConfig/database")
 const mongoose = require("mongoose")
 const mongoose_config = {useNewUrlParser: true, useUnifiedTopology: true}
@@ -31,20 +26,5 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use("/", userRoutes)
 app.use("/api", apiRoutes)
-
-// TEST Transaction stuff
-async function test(){
-    // await User.deleteMany({name: "g"})
-    const user = await User.findOne({email: "g@mail.com"})
-    console.log(user)
-    budgets = []
-    user.budgets = budgets
-    await user.save()
-    console.log(user.budgets)
-    // user.transactions.push({amount: 20, category: "Groceries", dateCreated: Date.now()})
-    // await user.save( () => console.log("saved") )
-}
-// test()
-// END TEST
 
 app.listen(port, () => console.log(`listening on port ... ${port}`))
